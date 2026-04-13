28438ea65eb47244ddf0ed9cd996cb3c6fea2172
pulkit-999 <pulkitagrawal777@gmail.com>
2026-04-13 16:28:01 +0530

    Added booking

---
+++ internal/booking/domain.go
@@ -0,0 +1,14 @@
+package booking
+
+type Booking struct {
+	ID      string
+	MovieID string
+	SeatID  string
+	UserID  string
+	Status  string
+}
+
+type BookingStore interface {
+	Book(b Booking) (Booking, error)
+	ListBookings(movieID string) []Booking
+}
